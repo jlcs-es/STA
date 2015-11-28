139c67f80a9b95916331c87d372f9114dde677f2
root <root@joseluis-VirtualBox>
2015-11-28 18:58:12 +0100

    owncloud php

---
+++ ORG1/SERVER/OWNCLOUD/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocwyfw5mr7zi',
+  'passwordsalt' => '5Our39RLt6WSJpQMifALpRCoscAoai',
+  'secret' => 'e9EL1WjyLwrArbgt+OXPVf8fqgfe+IpsF3VxU2rN01fdjxYo',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => '192.168.31.100',
+    2 => 'server.org31'
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '8.1.4.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
