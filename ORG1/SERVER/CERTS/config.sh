7c4b9f95f27e39b1ef6759085d6573ccecbe1649
Giuseppe Luigi <joseluis.canovas@outlook.com>
2015-12-06 23:01:45 +0100

    Añadido script para los certificados YA creados.

---
+++ ORG1/SERVER/CERTS/config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Copy openssl.cnf and demoCA
+
+cp ./openssl.cnf /usr/lib/ssl/openssl.cnf
+os.chdir("/etc/ldap/")
+bash("tar", ["-xzf", "initial.tar.gz"])
+tar -xzf demoCA.tar.gz -C /home/alumno/
+
+
+# Add apache site for CRL
+
+mkdir /var/www/crl
+cat > /etc/apache2/conf-available/crl.conf << EOF
+Alias /crl "/var/www/crl/"
+<Directory "/var/www/crl/">
+    Options +FollowSymLinks
+    AllowOverride All
+</Directory>
+
+EOF
+
+a2enconf crl.conf
+service apache2 reload
+
+
+# Add cron job to generate each day a new CRL list in /var/www/crl/org31.CRL and copy to web server
+crontab -l | { cat; echo "0 0 * * * openssl ca -keyfile /home/alumno/demoCA/private/cakey.pem -gencrl -out /var/www/crl/org31.CRL"; } | crontab -
