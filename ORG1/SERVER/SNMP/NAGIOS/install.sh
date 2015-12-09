5047795df2077c6231d605d9a549f6ed11ddf724
alumno <alumno@Laboratorio27>
2015-12-09 12:17:16 +0100

    NAGIOS

--- ORG1/SERVER/SNMP/NAGIOS/install.sh
+++ ORG1/SERVER/SNMP/NAGIOS/install.sh
@@ -1,1 +1,9 @@
 apt-get install -y nagios3
+
+# Poner contraseña alumno
+# Usuario por defecto: nagiosadmin
+
+# Problemas de DISCO, ir a disk.cfg y añadir al final de command_line de check_disk y check_all_disks
+
+# -A -i '.gvfs'
+
